285d3bca31dccb5559b3ec0395c5ad372545b5e1
WasinHongmanee <wasinhongmanee@gmail.com>
2022-01-24 03:03:39 -0800

    updated formats

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ import csv
 # The key the track is in. Integers map to pitches using standard Pitch Class notation. E.g. 0 = C, 1 = C♯/D♭, 2 = D, and so on. If no key was detected, the value is -1. [-1,11]
 # The popularity of a track is a value between 0 and 100, with 100 being the most popular.The popularity is calculated by algorithm and is based, in the most part,
 # on the total number of plays the track has had and how recent those plays are.
+
 def GetTracks(playlist: str) -> list:
     spotifyObject = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials())
     offset = 0
@@ -28,6 +29,7 @@ def GetTracks(playlist: str) -> list:
         for i in response['items']:
             playlist_songs.append(i['track']['id'])
         offset = offset + len(response['items'])
+        print('offset is: ', offset)
     return playlist_songs
 
 
@@ -35,11 +37,14 @@ def GetFeatures(songIDs: list) -> dict:
     sp = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials())
     sp.trace = True
     songFeatures = []
+    count = 0
     for i in songIDs:
+        count += 1
+        if count % 10 == 0:
+            print('Song number', count, 'added')
         artists = []
         artistlinks = []
         track = sp.track(i)
-
         for artist in track['artists']:  # make a list of artists names and convert them into one string separated by a comma
             artistlinks.append(artist['external_urls']['spotify'])
             artists.append(artist['name'])
@@ -53,9 +58,6 @@ def GetFeatures(songIDs: list) -> dict:
         tempdict.update({'popularity': track['popularity']})
         tempdict.update(features[0])  # eg acousticness, danceability
         songFeatures.append(tempdict)
-        # songfeatures.append({'name': track['name'], 'artist': track['artists'][0]['name']}, features[0])
-    #   songfeatures.append(features[0])
-    # pprint(songfeatures)
     return songFeatures
 
 
@@ -66,8 +68,12 @@ def ExportToCSV(features: dict):
                     'time_signature',
                     'track_href',
                     'type', 'uri', 'valence']
-    csv_name = 'sussy.csv'
-
+    if len(features) > 1:
+        csv_name = 'sussy.csv'
+        print('updated sussy')
+    else:
+        print('updated userTrack')
+        csv_name = 'userTrack.csv'
     with open(csv_name, 'w', newline='', encoding='utf-8') as f:
         w = csv.DictWriter(f, column_names)
         w.writeheader()
@@ -76,7 +82,11 @@ def ExportToCSV(features: dict):
 
 
 if __name__ == '__main__':
-    Tracks = GetTracks('https://open.spotify.com/playlist/1Qr4RR0Ys2dgu59UFTMUq9?si=ae9de72050f542ca')
-    features = GetFeatures(Tracks)
-    ExportToCSV(features)
+    #Tracks = GetTracks('https://open.spotify.com/playlist/22NJNtoxRmGdDWfSrQ9ve6?si=f8d653200c1b4843')
+    #features = GetFeatures(Tracks)
+    print('got playlist tracks')
+    #ExportToCSV(features)
+    currentUserTrack = GetFeatures(['5sysgWS6HwrfhHDnqMh27l'])
+    print('got features')
+    ExportToCSV(currentUserTrack)
 
